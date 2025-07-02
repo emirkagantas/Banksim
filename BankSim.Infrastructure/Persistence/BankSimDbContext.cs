948dc076bd52f5feb857122fa8c8f5ea41530dc4
Emir Kağan Taş <e.kagantas06@gmail.com>
2025-07-02 15:58:43 +0300

    Setup project structure and implement BankSimDbContext

---
+++ BankSim.Infrastructure/Persistence/BankSimDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BankSim.Domain.Entities;
+
+namespace BankSim.Infrastructure.Persistence
+{
+    public class BankSimDbContext : DbContext
+    {
+        public BankSimDbContext(DbContextOptions<BankSimDbContext> options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Account> Accounts => Set<Account>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.FromAccount)
+                .WithMany(a => a.SentTransactions)
+                .HasForeignKey(t => t.FromAccountId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.ToAccount)
+                .WithMany(a => a.ReceivedTransactions)
+                .HasForeignKey(t => t.ToAccountId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
